82575f863671e8da195dd218da8f2340e4b7cffa
Lourenço Gomes <leto.lg@gmail.com>
2020-10-14 13:28:30 +0100

    - Main Activity display numbers

--- app/src/main/java/ipca/example/calculator/MainActivity.kt
+++ app/src/main/java/ipca/example/calculator/MainActivity.kt
@@ -2,10 +2,12 @@ package ipca.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     // Model
     var result : Float = 0f
@@ -14,7 +16,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button1
+        button0.setOnClickListener(this)
+        button1.setOnClickListener(this)
+        button2.setOnClickListener(this)
+        button3.setOnClickListener(this)
+        button4.setOnClickListener(this)
+        button5.setOnClickListener(this)
+        button6.setOnClickListener(this)
+        button7.setOnClickListener(this)
+        button8.setOnClickListener(this)
+        button9.setOnClickListener(this)
 
     }
+
+    override fun onClick(view: View?) {
+        val button : Button = view as Button
+        textViewDisplay.text = textViewDisplay.text.toString().plus(button.text)
+    }
+
+
 }
