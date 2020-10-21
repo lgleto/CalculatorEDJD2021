4ac2d31de0ed835cab8e83f9aad5f740874e625e
Lourenço Gomes <leto.lg@gmail.com>
2020-10-21 11:27:16 +0100

    - Main Activity display numbers

--- app/src/main/java/ipca/example/calculator/MainActivity.kt
+++ app/src/main/java/ipca/example/calculator/MainActivity.kt
@@ -6,11 +6,52 @@ import android.view.View
 import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
 
+enum class Operator {
+    add,
+    divid,
+    subtract,
+    multiply
+}
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     // Model
-    var result : Float = 0f
+    var operand : Float = 0f
+    var operator : Operator? = null
+    var userIsInTheMiddleOfIntroduction = false
+
+    var operatorFun : (view:View)->Unit = { view ->
+        doOperation()
+        val button : Button = view as Button
+        operator = when (button.text) {
+            "+" -> Operator.add
+            "-" -> Operator.subtract
+            "/" -> Operator.divid
+            "*" -> Operator.multiply
+            else -> null
+        }
+        operand = textViewDisplay.text.toString().toFloat()
+    }
+
+    fun doOperation(){
+        operator?.let{ op->
+            when (op){
+                Operator.add -> operand += textViewDisplay.text.toString().toFloat()
+                Operator.multiply -> operand *= textViewDisplay.text.toString().toFloat()
+                Operator.subtract -> operand -= textViewDisplay.text.toString().toFloat()
+                Operator.divid -> operand /= textViewDisplay.text.toString().toFloat()
+            }
+            textViewDisplay.text = operand.toString()
+        }
+        userIsInTheMiddleOfIntroduction = false
+    }
+
+    fun clear() {
+        textViewDisplay.text = "0"
+        operand = 0f
+        operator = null
+        userIsInTheMiddleOfIntroduction = false
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,12 +67,38 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         button7.setOnClickListener(this)
         button8.setOnClickListener(this)
         button9.setOnClickListener(this)
+        buttonPoint.setOnClickListener(this)
+
+        buttonSum.setOnClickListener(operatorFun)
+        buttonMin.setOnClickListener(operatorFun)
+        buttonMul.setOnClickListener(operatorFun)
+        buttonDiv.setOnClickListener(operatorFun)
+
+        buttonEqual.setOnClickListener {
+            doOperation()
+        }
 
     }
 
     override fun onClick(view: View?) {
         val button : Button = view as Button
-        textViewDisplay.text = textViewDisplay.text.toString().plus(button.text)
+        if (userIsInTheMiddleOfIntroduction) {
+            if(button.text.equals(".")){
+                if(!textViewDisplay.text.contains(".")){
+                    textViewDisplay.text = textViewDisplay.text.toString().plus(button.text)
+                }
+                return
+            }
+            if (textViewDisplay.text.equals("0")){
+                textViewDisplay.text = button.text
+            }
+            else {
+                textViewDisplay.text = textViewDisplay.text.toString().plus(button.text)
+            }
+        }else {
+            textViewDisplay.text = button.text
+            userIsInTheMiddleOfIntroduction = true
+        }
     }
 
 
